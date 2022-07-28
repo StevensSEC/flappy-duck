8aed9248e71151e7f498ba263fe5367fa91bf597
David Carpenter <carpenterd777@gmail.com>
2022-07-28 19:52:56 -0400

    add the app entrypoint

---
+++ src/app.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+
+from models.event_loop import event_loop
+
+# Python main function. This is the entrypoint to the app
+if __name__ == "__main__":
+    print("Running Flappy Ducks...")
+    event_loop()
