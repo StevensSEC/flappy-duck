811d7a2c134c1061f4160476bc6a156683a7ba4a
David Carpenter <carpenterd777@gmail.com>
2022-07-28 19:52:37 -0400

    add event loop function

---
+++ src/models/event_loop.py
@@ -0,0 +1,30 @@
+import sys
+
+import pygame
+from pygame import event
+
+from models.player import Player
+from models.window import Window
+
+
+def event_loop() -> None:
+    print("Entering main game loop")
+    pygame.init()
+
+    window = Window()
+
+    player = Player([2, 2])
+    window.register_entity(player)
+
+    # Event loop: a loop that checks if new events have occurred and responds to them if they have
+    while True:
+        # check all the events sent from pygame
+        for _event in event.get():
+            # if a quit event was sent, end the process
+            if _event.type == pygame.QUIT:
+                print("Found QUIT event")
+                sys.exit()
+
+        # handle the window
+        window.clear()
+        window.update()
