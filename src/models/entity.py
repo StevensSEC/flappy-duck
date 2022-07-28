b12dc5097287cdc7bd232bc576e2d582d38567fe
David Carpenter <carpenterd777@gmail.com>
2022-07-28 19:51:47 -0400

    add player class

---
+++ src/models/entity.py
@@ -0,0 +1,51 @@
+import abc
+
+from pygame import Rect, Surface
+
+
+class Entity(metaclass=abc.ABCMeta):
+    """Interface for any entity that can be managed by the window (i.e. a projectile, the player, etc."""
+
+    @abc.abstractmethod
+    def move(self) -> None:
+        """Move the entity across the window."""
+        raise NotImplementedError
+
+    @abc.abstractmethod
+    def get_left(self) -> int:
+        """Return's the entity's left-most position."""
+        raise NotImplementedError
+
+    @abc.abstractmethod
+    def get_right(self) -> int:
+        """Return's the entity's right-most position."""
+        raise NotImplementedError
+
+    @abc.abstractmethod
+    def get_top(self) -> int:
+        """Return's the entity's top-most position."""
+        raise NotImplementedError
+
+    @abc.abstractmethod
+    def get_bottom(self) -> int:
+        """Return's the entity's bottom-most position."""
+        raise NotImplementedError
+
+    @abc.abstractmethod
+    def handle_window_border_x(self) -> None:
+        """Define's the entity's behavior when it reaches the x-border of the window."""
+        raise NotImplementedError
+
+    @abc.abstractmethod
+    def handle_window_border_y(self) -> None:
+        """Define's the entity's behavior when it reaches the y-border of the window."""
+        raise NotImplementedError
+
+    @abc.abstractmethod
+    def get_surface(self) -> Surface:
+        """Return the entity's internal Surface."""
+        raise NotImplementedError
+
+    @abc.abstractmethod
+    def get_rect(self) -> Rect:
+        """Return the entity's internal Rect."""
---
+++ src/models/player.py
@@ -0,0 +1,52 @@
+from typing import List
+
+from pygame import image
+
+from models.entity import Entity
+
+
+class Player(Entity):
+    """Represents the player."""
+
+    def __init__(self, speed: List[int]):
+        """Create a new Player."""
+        self._player_image = image.load("assets/ball.gif")
+        self._player_rect = self._player_image.get_rect()
+
+        if len(speed) != 2:
+            raise RuntimeError(f"Invalid speed. Expected list of length 2, got length {len(speed)}")
+        self.speed = speed
+
+    def move(self):
+        """Moves the player according to their current speed."""
+        self._player_rect = self._player_rect.move(self.speed)
+
+    def get_left(self):
+        """Returns the player's left-most position."""
+        return self._player_rect.left
+
+    def get_right(self):
+        """Returns the player's right-most position."""
+        return self._player_rect.right
+
+    def get_bottom(self):
+        """Returns the player's bottom-most position."""
+        return self._player_rect.bottom
+
+    def get_top(self):
+        """Returns the player's top-most position."""
+        return self._player_rect.top
+
+    def handle_window_border_x(self):
+        """Handles the behavior of the player's image if they reach the x-borders of the window."""
+        self.speed[0] = -self.speed[0]
+
+    def handle_window_border_y(self):
+        """Handles the behavior of the player's image if they reach the y-borders of the window."""
+        self.speed[1] = -self.speed[1]
+
+    def get_surface(self):
+        return self._player_image
+
+    def get_rect(self):
+        return self._player_rect
