603386b3b617b3e5bafe75b628ab7a0f117de0a1
David Carpenter <carpenterd777@gmail.com>
2022-07-31 16:49:23 -0400

    test

--- src/app.py
+++ src/app.py
@@ -2,6 +2,7 @@
 
 from controllers.event_loop import event_loop
 
+# Test
 # Python main function. This is the entrypoint to the app
 if __name__ == "__main__":
     print("Running Flappy Ducks...")
